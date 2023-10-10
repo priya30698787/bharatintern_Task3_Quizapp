cad4ba6cbe843df7d77daea50903bf8b63e8ae30
priya30698787 <aditipriya306@gmail.com>
2023-10-10 18:01:38 +0530

    project

---
+++ app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+       "What is APK in context of Android?",
+            "Which programming language is primarily used in android app development",
+            "which one is not a protocol of IOT?",
+            "What is the main purpose of an Android intent?",
+            "which file extension is used in Android layout files?"
+
+    };
+    public static String choices[][]={
+            {"Android Package File","Application Program Kernel","Android Programming Kit","Android Program Key"},
+            {"Java","C++","Python","Javascript"},
+            {"MQTT","HTTP","CoAP","TCP/IP"},
+            {"To display advertisements","To send data b/n components","To secure app's data","To create animation"},
+            {".java",".xml",".apk",".gradle"}
+
+    };
+    public static String rightanswer[]={
+            "Android Package File",
+            "Java",
+            "TCP/IP",
+            "To send data b/n components",
+            ".xml"
+
+    };
+}
